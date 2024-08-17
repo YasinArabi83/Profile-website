efb53eefa7d34aef92f62a558e45d75307cfda30
Yasin <yasinarabi1383@gmail.com>
2024-08-17 20:59:51 +0330

    feat: add contact page

--- resumes/urls.py
+++ resumes/urls.py
@@ -22,5 +22,6 @@ from MyResume import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
